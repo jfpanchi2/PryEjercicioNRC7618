de107e3543b62ced471156dd905f29dc6d355588
jfpanchi2 <jfpanchi2@espe.edu.ec>
2020-05-20 15:54:27 -0500

    Codigo Inicial

---
+++ src/test/java/com/panchi/ejercicio/PryEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panchi.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
